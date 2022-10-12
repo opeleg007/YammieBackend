6affc0ea0ed59c09aefb948da8461a239db467da
Or Peleg <opeleg007@gmail.com>
2022-10-12 20:21:41 +0300

    Added Constructors, Getters and Setters to Classes

--- src/main/java/com/example/yammiebackend/Model/PlacedOrder.java
+++ src/main/java/com/example/yammiebackend/Model/PlacedOrder.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class PlacedOrder {
@@ -19,4 +20,98 @@ public class PlacedOrder {
     @OneToMany
     private List<RestaurantItem> itemList;
 
+    public PlacedOrder() {
+        timeOfOrder = LocalDateTime.now();
+    }
+
+    public PlacedOrder(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        timeOfOrder = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getTimeOfOrder() {
+        return timeOfOrder;
+    }
+
+    public void setTimeOfOrder(LocalDateTime timeOfOrder) {
+        this.timeOfOrder = timeOfOrder;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<RestaurantItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<RestaurantItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    public void addItem(RestaurantItem item) {
+        this.itemList.add(item);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+        if (!(o instanceof PlacedOrder))
+            return false;
+        PlacedOrder placedOrder = (PlacedOrder) o;
+        return Objects.equals(this.id, placedOrder.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.firstName, this.lastName, this.email, this.timeOfOrder);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + this.id + ", first name='" + this.firstName +
+                '\'' + ", last name='" + this.lastName + '\'' + ", email='" + this.email +
+                '\'' + ", time of order='" + this.timeOfOrder +
+                '\'' + ", price='" + this.price + '\'' + '}';
+    }
 }
--- src/main/java/com/example/yammiebackend/Model/RestaurantItem.java
+++ src/main/java/com/example/yammiebackend/Model/RestaurantItem.java
@@ -11,7 +11,36 @@ public class RestaurantItem {
     private @Id @GeneratedValue Long id;
     private Double price;
     @ElementCollection
-    private Set<String> Changes;
+    private Set<String> changes;
 
+    public RestaurantItem() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Set<String> getChanges() {
+        return this.changes;
+    }
+
+    public void setChanges(Set<String> changes) {
+        this.changes = changes;
+    }
+
+    public void addChange(String change) {
+        this.changes.add(change);
+    }
 }
