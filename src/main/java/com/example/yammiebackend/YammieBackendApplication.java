fd7f4de4c3fd7235d17064648a6dcabb2d806a38
Or Peleg <opeleg007@gmail.com>
2022-10-12 19:10:25 +0300

    Initial commit

---
+++ src/main/java/com/example/yammiebackend/YammieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.yammiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YammieBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YammieBackendApplication.class, args);
+    }
+
+}
